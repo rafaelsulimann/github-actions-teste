675ec8acffb6c1ca6587c0ae79f82004e8c9d9b0
Rafael Sulimann <sulimannrafael@gmail.com>
2023-08-31 20:47:32 -0300

    Criação do projeto

---
+++ authuser/src/main/java/com/teste_github_actions/authuser/AuthuserApplication.java
@@ -0,0 +1,13 @@
+package com.teste_github_actions.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApplication.class, args);
+	}
+
+}
