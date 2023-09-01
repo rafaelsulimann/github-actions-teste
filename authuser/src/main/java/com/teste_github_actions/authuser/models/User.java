59d48577708b4e47baf12ef72d474024bb235ccd
Rafael Sulimann <sulimannrafael@gmail.com>
2023-08-31 21:17:12 -0300

    Teste2

--- authuser/src/main/java/com/teste_github_actions/authuser/models/User.java
+++ authuser/src/main/java/com/teste_github_actions/authuser/models/User.java
@@ -3,6 +3,7 @@ package main.java.com.teste_github_actions.authuser.models;
 public class User {
 
     private String name;
+    private String email;
 
     public String getName() {
         return name;
@@ -10,6 +11,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 }
