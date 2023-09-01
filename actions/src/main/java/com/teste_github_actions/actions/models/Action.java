dda377976631b7e0fa5c57647862232e81a5dc50
Rafael Sulimann <sulimannrafael@gmail.com>
2023-08-31 21:59:25 -0300

    teste11

---
+++ actions/src/main/java/com/teste_github_actions/actions/models/Action.java
@@ -0,0 +1,15 @@
+package com.teste_github_actions.actions.models;
+
+public class Action {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
